0e0f2ad13e37d44f57ef32097256b5472c1b19be
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-29 22:25:27 +0000

    Update MainCli.java

--- pow/src/main/java/com/alastairlundy/utils/pow/MainCli.java
+++ pow/src/main/java/com/alastairlundy/utils/pow/MainCli.java
@@ -5,8 +5,8 @@ import com.alastairlundy.utils.pow.commands.Power;
 import com.alastairlundy.utils.pow.commands.Root;
 import com.github.rvesse.airline.annotations.Cli;
 
-@Cli(name = "average",
-        description = "Calculate averages easily",
+@Cli(name = "pow",
+        description = "",
         defaultCommand = Root.class,
         commands = {Root.class, Power.class}
 )
