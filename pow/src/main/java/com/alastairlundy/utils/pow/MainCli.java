8770d669831f672bac8cbe4cd583700200fa36dd
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-29 22:23:53 +0000

    remove mocking for now

--- pow/src/main/java/com/alastairlundy/utils/pow/MainCli.java
+++ pow/src/main/java/com/alastairlundy/utils/pow/MainCli.java
@@ -1,15 +1,14 @@
 package com.alastairlundy.utils.pow;
 
-import com.alastairlundy.utils.average.commands.Mean;
-import com.alastairlundy.utils.average.commands.Median;
-import com.alastairlundy.utils.average.commands.Mode;
-import com.alastairlundy.utils.average.commands.Root;
+
+import com.alastairlundy.utils.pow.commands.Power;
+import com.alastairlundy.utils.pow.commands.Root;
 import com.github.rvesse.airline.annotations.Cli;
 
 @Cli(name = "average",
         description = "Calculate averages easily",
         defaultCommand = Root.class,
-        commands = {Mean.class, Root.class, Mode.class, Median.class}
+        commands = {Root.class, Power.class}
 )
 public class MainCli {
 
