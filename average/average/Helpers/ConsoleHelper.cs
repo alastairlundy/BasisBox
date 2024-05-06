43eec62ac9692db4d93d4b6c0513ad0d1858b035
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-05-06 22:27:56 +0100

    Create ConsoleHelper.cs

---
+++ average/average/Helpers/ConsoleHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace average.Helpers;
+
+internal class ConsoleHelper
+{
+    internal static decimal[] ConvertInputToDecimal(string[] values)
+    {
+        List<decimal> newValues = new List<decimal>();
+
+        foreach (string value in values)
+        {
+            newValues.Add(decimal.Parse(value));
+        }
+
+        return newValues.ToArray();
+    }
+
+    public static void PrintUnformattedStr(string str)
+    {
+        Console.WriteLine(str);
+    }
+
+    public static void PrintUnformattedStrArray(string[] array)
+    {
+        foreach (string value in array)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    public static void PrintVersion()
+    {
+
+    }
+
+    public static void PrintUnformattedLicense()
+    {
+
+
+        foreach (string line in File.ReadAllLines($"Environment.CurrentDirectory{Path.DirectorySeparatorChar}LICENSE.txt"))
+        {
+            Console.WriteLine(line);
+        }
+
+        Console.WriteLine();
+    }
+}
