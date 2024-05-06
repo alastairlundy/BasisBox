c0f017cc1d4a2c6a6cec805fd50a66d5fd5a539b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-05-06 20:40:12 +0100

    clean up code a bit

--- average/average/Program.cs
+++ average/average/Program.cs
@@ -4,68 +4,65 @@ using AlastairLundy.Extensions.System.VersionExtensions.Enums;
 using McMaster.Extensions.CommandLineUtils;
 using System.Reflection;
 
-namespace average
+namespace average;
+
+internal class Program
 {
-    internal class Program
+    static int Main(string[] args)
     {
-        static int Main(string[] args)
+        CommandLineApplication app = new CommandLineApplication();
+
+        var help = app.HelpOption("-h|--help");
+        var version = app.VersionOption("-v|--version", appVersion.GetFriendlyVersionToString(FriendlyVersionFormatStyle.MajorDotMinorDotBuild), appVersion.ToString());
+
+        app.Command("mean", meanCommand =>
         {
-            CommandLineApplication app = new CommandLineApplication();
+            var geometricFlag = meanCommand.Option("--geometric", "Calculate the geometric mean", CommandOptionType.SingleOrNoValue);
+            var arithmeticFlag = meanCommand.Option("-arithmetic", "Calculate the arithmetic mean.", CommandOptionType.SingleOrNoValue).DefaultValue = "true";
 
-            Version appVersion = Assembly.GetEntryAssembly()!.GetName().Version!;
+            var rounding = meanCommand.Option("-dp | --decimal-places", "", CommandOptionType.SingleValue).DefaultValue = "2";
 
-            var help = app.HelpOption("-h|--help");
-            var version = app.VersionOption("-v|--version", appVersion.GetFriendlyVersionToString(FriendlyVersionFormatStyle.MajorDotMinorDotBuild), appVersion.ToString());
+            var numbers = meanCommand.Argument("<numbers>", "The numbers to get the average of.", true);
 
-            app.Command("mean", meanCommand =>
+
+
+
+        });
+
+        app.Command("median", medianCommand =>
+        {
+
+        });
+
+        app.Command("mode", modeCommand =>
+        {
+
+        });
+
+        app.Command("midrange", midRangeCommand =>
+        {
+
+        });
+
+        app.Command("interquartilemean", iqrMean =>
+        {
+
+        });
+
+
+        app.OnExecute(() =>
+        {
+            if(version.HasValue()) 
             {
-                var geometricFlag = meanCommand.Option("--geometric", "Calculate the geometric mean", CommandOptionType.SingleOrNoValue);
-                var arithmeticFlag = meanCommand.Option("-arithmetic", "Calculate the arithmetic mean.", CommandOptionType.SingleOrNoValue).DefaultValue = "true";
+                
+            }
 
-                var rounding = meanCommand.Option("-dp | --decimal-places", "", CommandOptionType.SingleValue).DefaultValue = "2";
-
-                var numbers = meanCommand.Argument("<numbers>", "The numbers to get the average of.", true);
-
-
-
-
-            });
-
-            app.Command("median", medianCommand =>
+            if(help.HasValue())
             {
+                app.ShowHelp();
+            }
+        });
 
-            });
-
-            app.Command("mode", modeCommand =>
-            {
-
-            });
-
-            app.Command("midrange", midRangeCommand =>
-            {
-
-            });
-
-            app.Command("interquartilemean", iqrMean =>
-            {
-
-            });
-
-
-            app.OnExecute(() =>
-            {
-                if(version.HasValue()) 
-                {
-                    
-                }
-
-                if(help.HasValue())
-                {
-                    app.ShowHelp();
-                }
-            });
-
-            return app.Execute(args);
-        }
+        return app.Execute(args);
     }
 }
