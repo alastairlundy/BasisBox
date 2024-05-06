67fee2882a251dd4df5135d585457700804fcad4
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-05-06 22:27:11 +0100

    start moving text to localizations

--- average/average/Program.cs
+++ average/average/Program.cs
@@ -1,8 +1,15 @@
 ﻿using AlastairLundy.Extensions.System.VersionExtensions;
 using AlastairLundy.Extensions.System.VersionExtensions.Enums;
 
+using AlastairLundy.Extensions.System;
+
 using McMaster.Extensions.CommandLineUtils;
 using System.Reflection;
+
+using AlastairLundy.Extensions.System.AssemblyExtensions;
+using average.localizations;
+using average.Helpers;
+using System.ComponentModel.Design;
 
 namespace average;
 
@@ -12,20 +19,30 @@ internal class Program
     {
         CommandLineApplication app = new CommandLineApplication();
 
+        var appVersion = Assembly.GetExecutingAssembly().GetProjectVersion();
+
         var help = app.HelpOption("-h|--help");
-        var version = app.VersionOption("-v|--version", appVersion.GetFriendlyVersionToString(FriendlyVersionFormatStyle.MajorDotMinorDotBuild), appVersion.ToString());
+        var license = app.Option("-l|--license", Resources.Command_License_Description, CommandOptionType.NoValue);
+        var version = app.VersionOption("-v|--version", appVersion.GetFriendlyVersionToString(), appVersion.ToString());
 
         app.Command("mean", meanCommand =>
         {
-            var geometricFlag = meanCommand.Option("--geometric", "Calculate the geometric mean", CommandOptionType.SingleOrNoValue);
-            var arithmeticFlag = meanCommand.Option("-arithmetic", "Calculate the arithmetic mean.", CommandOptionType.SingleOrNoValue).DefaultValue = "true";
+            var geometricFlag = meanCommand.Option("--geometric", Resources.Command_GeometricMean_Description, CommandOptionType.SingleOrNoValue);
+            var arithmeticFlag = meanCommand.Option("-arithmetic", Resources.Command_ArithmeticMean_Description, CommandOptionType.SingleOrNoValue);
 
-            var rounding = meanCommand.Option("-dp | --decimal-places", "", CommandOptionType.SingleValue).DefaultValue = "2";
+            var decimalRounding = meanCommand.Option("-dp | --decimal-places", Resources.Rounding_DecimalPlaces_Description, CommandOptionType.SingleValue).DefaultValue = "2";
 
-            var numbers = meanCommand.Argument("<numbers>", "The numbers to get the average of.", true);
+            var numbers = meanCommand.Argument("<numbers>", Resources.Numbers_Description, true);
 
 
+            if (geometricFlag.HasValue())
+            {
 
+            }
+            else if (arithmeticFlag.HasValue())
+            {
+
+            }
 
         });
 
@@ -36,11 +53,28 @@ internal class Program
 
         app.Command("mode", modeCommand =>
         {
+            var modeHelp = modeCommand.HelpOption("-h|--help");
 
+            modeCommand.Description = Resources.Command_Mode_About;
+
+            var numbers = modeCommand.Argument("<numbers>", Resources.Numbers_Description, true);
+
+
+            modeCommand.OnExecute(() =>
+            {
+
+
+
+                if (modeHelp.HasValue())
+                {
+                    modeCommand.ShowHelp();
+                }
+            });
         });
 
         app.Command("midrange", midRangeCommand =>
         {
+            var rounding = midRangeCommand.Option("-r|--round", "", CommandOptionType.SingleValue);
 
         });
 
@@ -54,12 +88,16 @@ internal class Program
         {
             if(version.HasValue()) 
             {
-                
+                ConsoleHelper.PrintVersion();
             }
 
             if(help.HasValue())
             {
                 app.ShowHelp();
+            }
+            if (license.HasValue())
+            {
+                ConsoleHelper.PrintUnformattedLicense();
             }
         });
 
