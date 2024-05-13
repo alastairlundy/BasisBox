a0e590309729660cf42c3264d0244df0ed796ab5
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-05-13 22:36:09 +0100

    clean up Median command

--- Average/Average/Commands/CalculationCommands/MedianCommand.cs
+++ Average/Average/Commands/CalculationCommands/MedianCommand.cs
@@ -17,8 +17,9 @@
 using System.Globalization;
 using AlastairLundy.Extensions.System.Maths.Averages;
 using Average.Helpers;
-using Average.Library;
+
 using Average.Localizations;
+
 using Spectre.Console;
 using Spectre.Console.Cli;
 
