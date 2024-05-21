054365235dcfeabacc4319f7c5d01ecee7448087
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-05-21 12:43:34 +0100

    move string formatter code to separate library

---
+++ Parrot/Parrot.Library/StringFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+using AlastairLundy.Extensions.System.StringExtensions;
+
+namespace Parrot.Cli.Helpers;
+
+public class StringFormatter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static string RemoveEscapeChars(string input)
+    {
+        input.RemoveEscapeCharacters();
+        return input;
+    }
+}
