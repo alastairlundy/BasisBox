9b34f1fc54f6a202635576949f3631526a057329
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-06-15 13:32:05 +0100

    Parrot: start working on formatting

---
+++ Parrot/Parrot.Cli/ColourFormatParsing.cs
@@ -0,0 +1,42 @@
+﻿using Spectre.Console;
+
+namespace Parrot.Cli;
+
+public class ColourFormatParsing
+{
+
+    
+    public static Grid Format(string[] lines)
+    {
+        Grid grid = new();
+        
+        foreach (string line in lines)
+        {
+            string newLine = line;
+
+            Color? backgroundColor;
+            Color? foregroundColor;
+            
+            if (line.Contains("${") && line.Contains('}'))
+            {
+                newLine = newLine.Replace("$", string.Empty);
+                
+                if (newLine.Contains("BG"))
+                {
+                    newLine = newLine.Replace("BG", string.Empty);
+
+                    backgroundColor = GetColour(newLine);
+                }
+
+                if (newLine.Contains("FG"))
+                {
+                    newLine = newLine.Replace("FG", string.Empty);
+
+                    foregroundColor = GetColour(newLine);
+                }
+            }
+        }
+
+        return grid;
+    }
+}
