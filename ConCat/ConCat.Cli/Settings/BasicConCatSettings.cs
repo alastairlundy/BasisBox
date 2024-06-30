960fbf5a3dc3f2ad0e937e72cbb29bdd1c3419e9
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-06-30 20:32:05 +0100

    ConCat: create new common CommandSettings

---
+++ ConCat/ConCat.Cli/Settings/BasicConCatSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Spectre.Console.Cli;
+
+namespace ConCat.Cli.Settings;
+
+public class BasicConCatSettings : CommandSettings
+{
+    [CommandArgument(0, "<File(s)>")]
+    public string[]? Files { get; init; }
+
+    [CommandOption("-n")]
+    [DefaultValue(false)]
+    public bool AppendLineNumbers { get; init; }
+        
+    [CommandOption("--verbose|--debug")]
+    [DefaultValue(false)]
+    public bool ShowErrors { get; init; }
+}
