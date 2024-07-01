991dfffa5f1ffa57f9bad69ee7a6d914d60d9e37
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-07-01 21:41:14 +0100

    add Login Name

---
+++ LoginName/LoginName.Cli/Commands/LoginNameCommand.cs
@@ -0,0 +1,56 @@
+﻿/*
+     Copyright 2024 Alastair Lundy
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using System;
+using System.ComponentModel;
+
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace LoginName.Cli.Commands;
+
+public class LoginNameCommand : Command<LoginNameCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        
+        [CommandOption("--debug|--debugging")]
+        [DefaultValue(false)]
+        public bool UseDebugging { get; init; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        try
+        {
+            AnsiConsole.WriteLine(Environment.UserName);
+            return 0;
+        }
+        catch(Exception exception)
+        {
+            if (settings.UseDebugging)
+            {
+                AnsiConsole.WriteException(exception);
+            }
+            else
+            {
+                AnsiConsole.WriteException(exception, ExceptionFormats.NoStackTrace);
+            }
+            
+            return -1;
+        }
+    }
+}
---
+++ LoginName/LoginName.Cli/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+     Copyright 2024 Alastair Lundy
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using System.Reflection;
+
+using LoginName.Cli.Commands;
+using LoginName.Cli.Localizations;
+
+using Spectre.Console.Cli;
+
+CommandApp app = new();
+
+app.Configure(config =>
+{
+    config.AddCommand<LoginNameCommand>("name")
+        .WithAlias("")
+        .WithDescription(Resources.Commands_LoginName_Description);
+
+    config.SetApplicationName(Assembly.GetExecutingAssembly().GetName().Name!);
+    config.UseAssemblyInformationalVersion();
+});
+
+app.SetDefaultCommand<LoginNameCommand>();
+
+return app.Run(args);
