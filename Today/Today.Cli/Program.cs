1ae32e26a321a70d43bc2f0a4fc087de5a839a1a
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-07-08 13:58:11 +0100

    Update Program.cs

--- Today/Today.Cli/Program.cs
+++ Today/Today.Cli/Program.cs
@@ -29,7 +29,7 @@ app.Configure(config =>
     {
         configurator.AddCommand<DateCommand>("").WithAlias("date");
         
-        configurator.AddCommand<DisplayDateCommand>("display");
+//        configurator.AddCommand<DisplayDateCommand>("display");
         configurator.AddCommand<SetDateCommand>("set");
 
     });
