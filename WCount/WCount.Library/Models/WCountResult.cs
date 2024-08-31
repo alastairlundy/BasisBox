b6d26e14023a07167fa895d8b22ec385f054d3b9
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-08-31 19:08:05 +0100

    Create WCountResult.cs

---
+++ WCount/WCount.Library/Models/WCountResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCount.Library.Models
+{
+    public class WCountResult
+    {
+        public ulong WordCount { get; set; }
+        public ulong CharCount { get; set; }
+        public ulong ByteCount { get; set; }
+
+        public int LineCount { get; set; }
+    }
+}
