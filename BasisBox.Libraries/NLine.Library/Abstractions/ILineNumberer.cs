eddb9d998c0f91506d1972590a3d8c7a01bb388f
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-10-25 11:21:51 +0100

    add NLine and WCount libraries to BasisBox

---
+++ BasisBox.Libraries/NLine.Library/Abstractions/ILineNumberer.cs
@@ -0,0 +1,40 @@
+﻿/*
+    BasisBox - NLine Library
+    Copyright (C) 2024 Alastair Lundy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.Generic;
+
+namespace NLine.Library.Abstractions
+{
+    public interface ILineNumberer
+    { 
+        int CalculateNextLineNumber(int currentIndex, int lineIncrementor, int initialLineNumber);
+        
+        IEnumerable<string> AddLineNumbers(IEnumerable<string> lines, string lineNumberAppendedText);
+
+#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
+        #nullable enable
+        
+        IEnumerable<string> AddLineNumbers(IEnumerable<string> lines, int lineIncrementor, int initialLineNumber,
+            string lineNumberAppendedText, bool assignEmptyLinesANumber, int numberOfEmptyLinesToGroupTogether,
+            int columnNumber, bool tabSpaceAfterLineNumber, bool addLeadingZeroes, string? listNumbersWithString = null);
+#else
+        IEnumerable<string> AddLineNumbers(IEnumerable<string> lines, int lineIncrementor, int initialLineNumber,
+            string lineNumberAppendedText, bool assignEmptyLinesANumber, int numberOfEmptyLinesToGroupTogether,
+            int columnNumber, bool tabSpaceAfterLineNumber, bool addLeadingZeroes, string listNumbersWithString = null);
+#endif
+    }
+}
---
+++ BasisBox.Libraries/WCount.Library/CharCounter.cs
@@ -0,0 +1,81 @@
+﻿/*
+    BasisBox - WCount Library
+    Copyright (C) 2024 Alastair Lundy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using WCount.Library.Interfaces;
+using WCount.Library.Localizations;
+
+namespace WCount.Library;
+
+public class CharCounter : ICharCounter
+{
+    /// <summary>
+    /// Get the number of characters in a string.
+    /// </summary>
+    /// <param name="s">The string to be searched.</param>
+    /// <returns>the number of characters in a string.</returns>
+    public ulong CountCharacters(string s)
+    {
+        ulong totalChars = Convert.ToUInt64(s.ToCharArray().Length);
+
+        return totalChars;
+    }
+
+    /// <summary>
+    /// Gets the number of characters in a file.
+    /// </summary>
+    /// <param name="filePath">The file path of the file to be searched.</param>
+    /// <returns>the number of characters in the file specified.</returns>
+    /// <exception cref="FileNotFoundException">Thrown if the file specified could not be found.</exception>
+    public ulong CountCharactersInFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            ulong totalChars = 0;
+            
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                totalChars += CountCharacters(line.Split(' '));
+            }
+
+            return totalChars;
+        }
+
+        throw new FileNotFoundException(Resources.Exceptions_FileNotFound_Message, filePath);
+    }
+
+    /// <summary>
+    /// Gets the number of characters in an IEnumerable of strings.
+    /// </summary>
+    /// <param name="enumerable">The IEnumerable to be searched.</param>
+    /// <returns>the number of characters in the specified IEnumerable.</returns>
+    public ulong CountCharacters(IEnumerable<string> enumerable)
+    {
+        ulong totalChars = 0;
+
+        foreach (string s in enumerable)
+        {
+            totalChars += Convert.ToUInt64(s.ToCharArray().Length);
+        }
+
+        return totalChars;
+    }
+}
